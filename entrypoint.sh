d9f1630a6632eb58e14547630fed5789bdcd42a8
Wim Fournier <wim@fournier.nl>
2017-05-14 00:32:02 +0200

    added debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,13 +13,21 @@ shift
 token=""
 if ([ -s /secrets/vault ] &&  $(echo $GIT|grep -q @))
 then
+	echo "Adding private token to git URL"
 	. /secrets/vault
 	prefix=$(echo $GIT|cut -d@ -f1)
 	suffix=$(echo $GIT|cut -d@ -f2-)
 	GIT="$prefix:$token@$suffix"
 fi
 
+echo "Cloning git repo"
 git clone "${GIT}${TOKEN}" /build
 cd /build
-test -f requirements.txt && pip install -r requirements.txt
+if ([ -f requirements.txt ])
+then
+	echo "Installing python requirements"
+	cat requirements.txt
+	pip install -r requirements.txt
+fi
+echo "Starting main program $PROG $@"
 python "$PROG" $@
